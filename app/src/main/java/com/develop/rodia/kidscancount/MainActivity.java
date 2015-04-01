b5f1b727891f97b534cbcf52249db73eb8ae6909
César Israel Jaldin Peña <rodia.piedra@gmail.com>
2015-04-01 16:04:10 -0400

    Initial commit

---
+++ app/src/main/java/com/develop/rodia/kidscancount/MainActivity.java
@@ -0,0 +1,104 @@
+package com.develop.rodia.kidscancount;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            Button button = (Button) rootView.findViewById(R.id.button_activity);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(getActivity(), CreateActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            button = (Button) rootView.findViewById(R.id.button_hit);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(getActivity(), HitActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            button = (Button) rootView.findViewById(R.id.button_award);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(getActivity(), AwardActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            button = (Button) rootView.findViewById(R.id.button_phyco);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(getActivity(), PycoActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            return rootView;
+        }
+    }
+}
